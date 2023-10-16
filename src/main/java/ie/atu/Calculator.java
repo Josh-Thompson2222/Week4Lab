358c214b23dd2bd9e67fd3ba39210fa54f2d0c53
Josh <G00422756@atu.ie>
2023-10-16 17:30:37 +0100

    Added a new method called divide

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -7,6 +7,7 @@ public class Calculator {
         System.out.println("Let's Finish early");
         add();
         delete();
+        division();
     }
 
 
@@ -34,6 +35,19 @@ public class Calculator {
 
         int total = firstNumber - secondNumber;
         System.out.println("The total subtracted is " + total);
+    }
+
+    public static void division()
+    {
+        System.out.println("Please enter your first number : ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number : ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber / secondNumber;
+        System.out.println("The total divided is " + total);
     }
 
 }
